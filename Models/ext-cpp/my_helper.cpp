8e5915a09773c8f862b95760ee13c16541c79136
Aaronfang123 <1332752797@qq.com>
2022-03-08 13:54:43 +0800

    [feature] add stack helper for event

--- Models/ext-cpp/my_helper.cpp
+++ Models/ext-cpp/my_helper.cpp
@@ -1,14 +1,71 @@
 #include "my_helper.hpp"
 
+static int *stack;
+static int stack_size;
+static int top_element = -1;
+
+/**
+random num gen
+*/
 int get_rand() {
-	
-	// if((rand() % 100) == 0) {
-	// 	return rand() % 100;
-	// }
 	return rand() % 255; 
 }
 
 void init_seed() {
 	srand((int)time(NULL));  
 }
+
+/**
+stack helper
+*/
+
+bool is_empty(void)
+{
+    return top_element == -1;
+}
+
+
+bool is_full(void)
+{
+    return top_element == stack_size - 1;
+}
+
+// creat_stack
+void create_stack(int size)
+{
+    assert(stack_size == 0);
+    stack_size = size;
+    stack = (int*)malloc(stack_size * sizeof(int));
+    assert(stack != NULL);
+}
+
+void destroy_stack(void)
+{
+    assert(stack_size > 0);
+    stack_size = 0;
+    free(stack);
+    stack = NULL;
+}
+
+void push(int value)
+{
+    assert(!is_full());
+    top_element += 1;
+    stack[top_element] = value;
+}
+
+int pop(void)
+{
+    assert(!is_empty());
+    return stack[top_element--];
+}
+
+int top(void)
+{
+    assert(!is_empty());
+    return stack[top_element];
+}
+
+
+
 	
--- Models/ext-cpp/my_helper.hpp
+++ Models/ext-cpp/my_helper.hpp
@@ -1,6 +1,32 @@
 #include <cstdlib>
 #include <ctime>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <malloc.h>
 
+/**
+random num gen
+*/
 int get_rand();
 
 void init_seed();
+
+/**
+stack helper
+*/
+
+void push(int value);
+
+int pop(void);
+
+int top(void);
+
+bool is_empty(void);
+
+bool is_full(void);
+
+void create_stack(int size);
+
+void destroy_stack(void);
+
